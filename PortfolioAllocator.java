a46443c468905e3ae674d5b53615a06b4f33c515
haifa-mk <hohowww59@gmail.com>
2024-02-20 19:29:58 +0300

    added the methods to portfolio class and changed varibles names

--- PortfolioAllocator.java
+++ PortfolioAllocator.java
@@ -29,4 +29,31 @@ public class PortfolioAllocator {
     }
 
 
+
+
+    public static List<List<Integer>> assestAllocations(int targetSum, int[] maxValues) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateAllocation(targetSum, maxValues, new ArrayList<>());
+        return result;
+    }
+
+    public static void generateAllocation(int numOfAssets, int[] quantities, List<Integer> currentAllocation) {
+        if (currentAllocation.size() == quantities.length) {
+            int sum = 0;
+            for (int num : currentAllocation) {
+                sum += num;
+            }
+            if (sum == numOfAssets) {
+              //find_optimal
+            }
+            return;
+        }
+
+        for (int i = 0; i <= quantities[currentAllocation.size()]; i++) {
+            currentAllocation.add(i);
+            generateAllocation(numOfAssets, quantities, currentAllocation);
+            currentAllocation.remove(currentAllocation.size() - 1);
+        }
+    }
+
 }
