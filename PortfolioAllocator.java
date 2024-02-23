3fc42ebb98878385b6cb483ae851ca53c52353f2
Leenmaj <137584626+Leenmaj@users.noreply.github.com>
2024-02-23 18:49:46 +0300

    CHANGES

--- PortfolioAllocator.java
+++ PortfolioAllocator.java
@@ -6,17 +6,13 @@ static double bestReturn = -1; //any val
 static double bestRisk = Double.MAX_VALUE; // initialise with a high value
 static Scanner input = new Scanner(System.in);
    static List<Asset> assets = new ArrayList<>();
-   static double totalInvestment;
-   static double riskTolerance; static int no;
+   static int totalInvestment;
+   static double riskTolerance; 
+   static int no;
 
 
 public static void main(String[] args) {
-    getAllInputs();
-}
-
-
-
-    public static void getAllInputs() {
+  
         System.out.println("enter the no of assets:");
        no = input.nextInt();
         for (int i = 0; i < no; i++) {
@@ -29,34 +25,40 @@ public static void main(String[] args) {
             assets.add(asset);
         }
         System.out.println("Enter total investment amount:");
-        totalInvestment = input.nextDouble();
+        totalInvestment = input.nextInt();
         System.out.println("Enter risk tolerance level:");
         riskTolerance = input.nextDouble();
+
         int[] maxValues = new int[no]; // Initialise the array with the size of the assets list
     
         // Fill maxValues with the quantity of each asset
         for (int i = 0; i < no; i++) {
             maxValues[i] = assets.get(i).quantity;
         }
-       generateAllocation(no, maxValues,  new ArrayList<>());
-    
+       generateAllocation(totalInvestment, maxValues,  new ArrayList<>());
+    System.out.println("Optimal Allocation:  ");
+  for(int i = 0 ; i < no ; i++)
+  System.out.println(assets.get(i).id +": "+ bestAllocation.get(i)+ "units");
+  System.out.println("Expected Portfolio Return: "+bestReturn);
+    System.out.println("Expected Portfolio Risk: "+bestRisk);
     }
+    
     public void findOptimal(){
         //code
     }
 
 
 
-    public static void generateAllocation(int numOfAssets, int[] quantities, List<Integer> currentAllocation) {
+    public static void generateAllocation(int investmentAmount, int[] quantities, List<Integer> currentAllocation) {
 
         if (currentAllocation.size() == quantities.length) {
             int sum = 0;
             for (int num : currentAllocation) {
                 sum += num;
             }
-            if (sum == numOfAssets) {
-            	  double currentReturn = calculatePortfolio(currentAllocation,false, numOfAssets);
-                double currentRisk = calculatePortfolio(currentAllocation, true , numOfAssets);
+            if (sum == investmentAmount) {
+            	  double currentReturn = calculatePortfolio(currentAllocation,false, investmentAmount);
+                double currentRisk = calculatePortfolio(currentAllocation, true , investmentAmount);
                 if (currentReturn > bestReturn && currentRisk <= riskTolerance) {
                     bestReturn = currentReturn;
                     bestRisk = currentRisk;
@@ -67,26 +69,24 @@ public static void main(String[] args) {
 
         for (int i = 0; i <= quantities[currentAllocation.size()]; i++) {
             currentAllocation.add(i);
-            generateAllocation(numOfAssets, quantities, currentAllocation);
+            generateAllocation(investmentAmount, quantities, currentAllocation);
             currentAllocation.remove(currentAllocation.size() - 1);
         }
     }
 
 }
-public static double calculatePortfolio(List<Integer> allocatedAssest ,boolean isRiskLevel ,int numOfAssets) {
-        double Result= 0;
-        double weight;
-   
-        for (int i = 0; i < allocatedAssest.size(); i++) {
-            weight = (double)allocatedAssest.get(i) /(double) numOfAssets;
-            if(isRiskLevel){
-            Result += weight * assets.get(i).riskLevel ;
-            return Result ;}
-            else{
-            Result += weight * assets.get(i).expectedReturn ;
-            return Result ;}
-          }
-           return 0;
- 
-    } 
+public static double calculatePortfolio(List<Integer> allocatedAssets, boolean isRiskLevel, int totalUnits) {
+    double result = 0;
+    double weight;
+
+    for (int i = 0; i < allocatedAssets.size(); i++) {
+        weight = (double) allocatedAssets.get(i) / (double) totalUnits; 
+        if (isRiskLevel) {
+            result += weight * assets.get(i).riskLevel;
+        } else {
+            result += weight * assets.get(i).expectedReturn;
+        }
+    }
+    return result; 
+}
 }
